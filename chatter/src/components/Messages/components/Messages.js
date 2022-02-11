761fe4d86c7d61d0e25f272aca6fff5abb09968a
lucassodresa <lucas.sodresa@gmail.com>
2022-02-11 13:40:41 +0000

    solution of chatter

--- chatter/src/components/Messages/components/Messages.js
+++ chatter/src/components/Messages/components/Messages.js
@@ -1,30 +1,88 @@
-import React, { useContext } from 'react';
-import io from 'socket.io-client';
-import useSound from 'use-sound';
-import config from '../../../config';
-import LatestMessagesContext from '../../../contexts/LatestMessages/LatestMessages';
-import TypingMessage from './TypingMessage';
-import Header from './Header';
-import Footer from './Footer';
-import Message from './Message';
-import '../styles/_messages.scss';
+import React, {
+  useCallback,
+  useContext,
+  useEffect,
+  useState,
+  useRef,
+} from "react";
+import io from "socket.io-client";
+// import useSound from "use-sound";
+import config from "../../../config";
+import LatestMessagesContext from "../../../contexts/LatestMessages/LatestMessages";
+import TypingMessage from "./TypingMessage";
+import Header from "./Header";
+import Footer from "./Footer";
+import Message from "./Message";
+import "../styles/_messages.scss";
 
-const socket = io(
-  config.BOT_SERVER_ENDPOINT,
-  { transports: ['websocket', 'polling', 'flashsocket'] }
-);
+import initialBottyMessage from "../../../common/constants/initialBottyMessage";
+
+const socket = io(config.BOT_SERVER_ENDPOINT, {
+  transports: ["websocket", "polling", "flashsocket"],
+});
 
 function Messages() {
-  const [playSend] = useSound(config.SEND_AUDIO_URL);
-  const [playReceive] = useSound(config.RECEIVE_AUDIO_URL);
   const { setLatestMessage } = useContext(LatestMessagesContext);
+  const [messages, setMessages] = useState([
+    { user: "bot", message: initialBottyMessage },
+  ]);
+  const [botIsTyping, setBotIsTyping] = useState(false);
+  const [message, setMessage] = useState("");
+  const messagesEndRef = useRef(null);
+
+  const scrollToBottom = useCallback(() => {
+    messagesEndRef.current?.scrollIntoView({ behavior: "smooth" });
+  }, []);
+
+  const onChangeMessage = useCallback(({ target }) => {
+    setMessage(target.value);
+  }, []);
+
+  const sendMessage = useCallback(() => {
+    setMessages((messages) => [...messages, { user: "me", message }]);
+    socket.emit("user-message", message);
+    setMessage("");
+  }, [message]);
+
+  useEffect(() => {
+    socket.on("bot-typing", () => {
+      setBotIsTyping(true);
+    });
+
+    socket.on("bot-message", (message) => {
+      setBotIsTyping(false);
+      setMessages((messages) => [...messages, { user: "bot", message }]);
+    });
+  }, []);
+
+  useEffect(() => {
+    const lastPos = messages.length - 1;
+    const lastMessage = messages[lastPos];
+    setLatestMessage("bot", lastMessage.message);
+    scrollToBottom();
+  }, [messages, setLatestMessage, scrollToBottom]);
 
   return (
     <div className="messages">
       <Header />
       <div className="messages__list" id="message-list">
+        {messages.map((message, id) => {
+          return (
+            <Message
+              key={id}
+              botTyping={botIsTyping}
+              message={{ ...message, id }}
+            />
+          );
+        })}
+        {botIsTyping && <TypingMessage />}
+        <div ref={messagesEndRef} />
       </div>
-      <Footer message={message} sendMessage={sendMessage} onChangeMessage={onChangeMessage} />
+      <Footer
+        message={message}
+        sendMessage={sendMessage}
+        onChangeMessage={onChangeMessage}
+      />
     </div>
   );
 }
